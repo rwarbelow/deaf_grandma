b6fe07dcdb87c9e48b4f87da19dbf2389d631934
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-16 19:27:55 -0500

    done

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,13 +4,12 @@ get '/' do
   erb :index
 end
 
-get '/grandma' do
-  user_input = params[:user_input]
-  if user_input.downcase == user_input
+post '/grandma' do
+  if params[:user_input] == params[:user_input].downcase
     @grandma = "I CAN'T HEAR YOU!"
   else
     @grandma = "NOT SINCE 1969!"
   end
-  # erb :index
-  redirect to("/?grandma=#{@grandma}")
+   erb :index, layout: !request.xhr?
+  #redirect to("/?grandma=#{@grandma}")
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,16 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  $("#formid").submit(function(event){
+    event.preventDefault();
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+      $.ajax({
+      type: "POST",
+      url: "/grandma",
+      data: $("#formid").serialize();
+      success: function(data) {
+        $("body").html(data);
+      },
+    }
+      });
+  });
 });
+
